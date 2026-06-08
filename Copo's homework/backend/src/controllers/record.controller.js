15bcdab6e0bddd41d79fb40836ad5ccb9027b976
CarlosMariommm <20210212@ricaldone.edu.sv>
2026-06-08 12:37:34 -0600

    Actualizar record.controller.js

--- Copo's homework/backend/src/controllers/record.controller.js
+++ Copo's homework/backend/src/controllers/record.controller.js
@@ -4,11 +4,9 @@ const recordController = {};
 
 recordController.create = async (req, res) => {
     try {
-        //#1- Creo un nuevo expediente clínico
         const newRecord = new ClinicalRecord(req.body);
         await newRecord.save();
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: newRecord });
     } catch (error) {
         console.log("error" + error);
@@ -18,11 +16,9 @@ recordController.create = async (req, res) => {
 
 recordController.getAll = async (req, res) => {
     try {
-        //#1- Busco todos los expedientes poblando la información del paciente
         const records = await ClinicalRecord.find()
             .populate('patient_id', 'name lastName email');
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: records });
     } catch (error) {
         console.log("error" + error);
@@ -32,12 +28,10 @@ recordController.getAll = async (req, res) => {
 
 recordController.getById = async (req, res) => {
     try {
-        //#1- Busco el expediente por ID
         const record = await ClinicalRecord.findById(req.params.id)
             .populate('patient_id', 'name lastName email');
         if (!record) return res.status(404).json({ message: "Action failed", error: "Expediente no encontrado" });
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: record });
     } catch (error) {
         console.log("error" + error);
@@ -47,11 +41,9 @@ recordController.getById = async (req, res) => {
 
 recordController.update = async (req, res) => {
     try {
-        //#1- Actualizo el expediente clínico
         const updatedRecord = await ClinicalRecord.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!updatedRecord) return res.status(404).json({ message: "Action failed", error: "Expediente no encontrado" });
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: updatedRecord });
     } catch (error) {
         console.log("error" + error);
@@ -61,11 +53,9 @@ recordController.update = async (req, res) => {
 
 recordController.delete = async (req, res) => {
     try {
-        //#1- Elimino el expediente
         const deletedRecord = await ClinicalRecord.findByIdAndDelete(req.params.id);
         if (!deletedRecord) return res.status(404).json({ message: "Action failed", error: "Expediente no encontrado" });
         
-        //#2- Respondo
         return res.json({ message: "Action done" });
     } catch (error) {
         console.log("error" + error);
