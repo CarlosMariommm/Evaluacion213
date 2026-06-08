1539315978ec003e6fd0ca95da7d46d71f09a9ec
CarlosMariommm <20210212@ricaldone.edu.sv>
2026-06-08 12:36:50 -0600

    Actualizar specialty.controller.js

--- Copo's homework/backend/src/controllers/specialty.controller.js
+++ Copo's homework/backend/src/controllers/specialty.controller.js
@@ -4,11 +4,9 @@ const specialtyController = {};
 
 specialtyController.create = async (req, res) => {
     try {
-        //#1- Creo una nueva especialidad con los datos del body
         const newSpecialty = new MedicalSpecialty(req.body);
         await newSpecialty.save();
         
-        //#2- Respondo al cliente
         return res.json({ message: "Action done", data: newSpecialty });
     } catch (error) {
         console.log("error" + error);
@@ -18,10 +16,8 @@ specialtyController.create = async (req, res) => {
 
 specialtyController.getAll = async (req, res) => {
     try {
-        //#1- Busco todas las especialidades
         const specialties = await MedicalSpecialty.find();
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: specialties });
     } catch (error) {
         console.log("error" + error);
@@ -31,11 +27,9 @@ specialtyController.getAll = async (req, res) => {
 
 specialtyController.getById = async (req, res) => {
     try {
-        //#1- Busco especialidad por ID
         const specialty = await MedicalSpecialty.findById(req.params.id);
         if (!specialty) return res.status(404).json({ message: "Action failed", error: "Especialidad no encontrada" });
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: specialty });
     } catch (error) {
         console.log("error" + error);
@@ -45,11 +39,9 @@ specialtyController.getById = async (req, res) => {
 
 specialtyController.update = async (req, res) => {
     try {
-        //#1- Actualizo la especialidad
         const updatedSpecialty = await MedicalSpecialty.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!updatedSpecialty) return res.status(404).json({ message: "Action failed", error: "Especialidad no encontrada" });
         
-        //#2- Respondo
         return res.json({ message: "Action done", data: updatedSpecialty });
     } catch (error) {
         console.log("error" + error);
@@ -59,11 +51,9 @@ specialtyController.update = async (req, res) => {
 
 specialtyController.delete = async (req, res) => {
     try {
-        //#1- Elimino la especialidad
         const deletedSpecialty = await MedicalSpecialty.findByIdAndDelete(req.params.id);
         if (!deletedSpecialty) return res.status(404).json({ message: "Action failed", error: "Especialidad no encontrada" });
         
-        //#2- Respondo
         return res.json({ message: "Action done" });
     } catch (error) {
         console.log("error" + error);
