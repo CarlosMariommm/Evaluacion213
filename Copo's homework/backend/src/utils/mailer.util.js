d1461c1261959db5cf48c81060004a2cb1ed6051
CarlosMariommm <20210212@ricaldone.edu.sv>
2026-06-08 12:34:43 -0600

    Actualizar mailer.util.js

--- Copo's homework/backend/src/utils/mailer.util.js
+++ Copo's homework/backend/src/utils/mailer.util.js
@@ -2,9 +2,8 @@ import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-//#1- Configuro el transportador de nodemailer usando el email y contraseña de entorno
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // Asumo Gmail porque el correo proporcionado termina en gmail.com
+    service: 'gmail', 
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.USER_PASSWORD
@@ -13,7 +12,6 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (to, subject, text, html) => {
     try {
-        //#2- Configuro y envío el correo electrónico
         const info = await transporter.sendMail({
             from: `"Hospital Rosales" <${process.env.USER_EMAIL}>`,
             to: to,
